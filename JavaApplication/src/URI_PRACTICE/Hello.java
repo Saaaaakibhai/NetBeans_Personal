480eabbed4aadc15577863ee9dd580407e14ffbd
Saaaaakibhai <69164727+Saaaaakibhai@users.noreply.github.com>
2021-10-23 00:17:27 +0600

    Personal NetBean Code

---
+++ JavaApplication/src/URI_PRACTICE/Hello.java
@@ -0,0 +1,12 @@
+package URI_PRACTICE;
+public class Hello {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        //int x = (a+b)*(a+b);
+        //build in
+        double x = Math.pow(a+b,2);
+        System.out.println(x);
+    }
+    
+}
